c4833c801d8b4fea33345699cd2745dd060525b0
Trinh Thanh Phuong <iphuongtt@gmail.com>
2018-12-25 22:55:45 +0700

    test3

---
+++ test3.php
@@ -0,0 +1,167 @@
+<?php
+${"GLOBALS"}["rppwle"]       = "turkis";
+${"GLOBALS"}["ozhdzhjuqm"]   = "tpoin";
+${"GLOBALS"}["hjvfncish"]    = "second";
+${"GLOBALS"}["gciigh"]       = "jvid";
+${"GLOBALS"}["haqjbezwtre"]  = "rpoin";
+${"GLOBALS"}["hunilbswnj"]   = "msg";
+${"GLOBALS"}["tqwgkdpgw"]    = "vinfo";
+${"GLOBALS"}["fqiqdtffn"]    = "vhttp";
+${"GLOBALS"}["vdkrnykoidd"]  = "header";
+${"GLOBALS"}["abfeuk"]       = "konfir";
+${"GLOBALS"}["fkjnalv"]      = "earn";
+${"GLOBALS"}["qqqrbbeyuls"]  = "pcode";
+${"GLOBALS"}["mmhtluk"]      = "js";
+${"GLOBALS"}["yklfldlhi"]    = "lpoin";
+${"GLOBALS"}["iwubhgkpyhn"]  = "ch";
+$oesvize                     = "ch";
+${"GLOBALS"}["bseinyhrik"]   = "red";
+${"GLOBALS"}["txniqoiut"]    = "email";
+${"GLOBALS"}["miknrswr"]     = "invite";
+${"GLOBALS"}["bditqkdgmk"]   = "uid";
+${"GLOBALS"}["jfmliyqkqv"]   = "user";
+${"GLOBALS"}["otwewgry"]     = "http";
+${"GLOBALS"}["qkzuumgkojl"]  = "result";
+${"GLOBALS"}["rkonvnr"]      = "jres";
+${"GLOBALS"}["xwmaeugvkll"]  = "info";
+${"GLOBALS"}["dfwqhrn"]      = "access_token";
+${"GLOBALS"}["jfxbmbehrevb"] = "pcode";
+${"GLOBALS"}["qajrktmxr"]    = "earn";
+${"GLOBALS"}["uyllmpi"]      = "curl";
+${"GLOBALS"}["lxrbwmtffgc"]  = "email";
+${"GLOBALS"}["xpivgmirdgk"]  = "head";
+$qejwhiicd                   = "pinfo";
+${"GLOBALS"}["lyxopjxorfi"]  = "video";
+${"GLOBALS"}["esqiepoy"]     = "link";
+${"GLOBALS"}["xsbepanksk"]   = "putih";
+${"GLOBALS"}["sehhepb"]      = "ch";
+${"GLOBALS"}["vuqdxzfzofi"]  = "t";
+${"GLOBALS"}["ktcmrjpgg"]    = "kuning";
+$cwilbt                      = "pinfo";
+${"GLOBALS"}["gympxomfxx"]   = "ijo";
+${"GLOBALS"}["zwllmooqrysu"] = "ijo";
+${"GLOBALS"}["ulllnbw"]      = "biru";
+$yhxbrunnkluc                = "t";
+${"GLOBALS"}["imqpetf"]      = "ch";
+$fechbx                      = "turkis";
+@system("clear");
+${"GLOBALS"}["ojbgwjoy"]        = "red";
+${"GLOBALS"}["jgntsehblf"]      = "curl";
+$vdsvvt                         = "pink";
+${"GLOBALS"}["lleqoojpla"]      = "putih";
+${"GLOBALS"}["axuccgfd"]        = "putih";
+${"GLOBALS"}["uisgibtwzn"]      = "ijo";
+${${"GLOBALS"}["ulllnbw"]}      = "[1;34m";
+${$fechbx}                      = "[1;36m";
+$bwltzjkybw                     = "head";
+${${"GLOBALS"}["zwllmooqrysu"]} = "[92m";
+${"GLOBALS"}["wypwuqut"]        = "jres";
+${${"GLOBALS"}["axuccgfd"]}     = "[1;37m";
+${"GLOBALS"}["gxfngkrscu"]      = "jres";
+${$vdsvvt}                      = "[1;35m";
+${"GLOBALS"}["roiebxnsxa"]      = "head";
+${${"GLOBALS"}["ojbgwjoy"]}     = "[1;31m";
+${${"GLOBALS"}["ktcmrjpgg"]}    = "[1;33m";
+${${"GLOBALS"}["vuqdxzfzofi"]}  = "\n";
+$tdhaqebtcem                    = "js";
+$ifzjhxmg                       = "access_token";
+$fwvtmngl                       = "header";
+print ${${"GLOBALS"}["gympxomfxx"]} . "\n__     _______ _____ _   _ \n\ \\   / / ____| ____| | | | " . ${${"GLOBALS"}["xsbepanksk"]} . "Phiên Bản: NEW" . ${${"GLOBALS"}["zwllmooqrysu"]} . "\n \ \ / /|  _| |  _| | | | | " . ${${"GLOBALS"}["xsbepanksk"]} . "TOOL VIP" . ${${"GLOBALS"}["zwllmooqrysu"]} . "\n  \ V / | |___| |___| |_| | " . ${${"GLOBALS"}["lleqoojpla"]} . "X3 Coin" . ${${"GLOBALS"}["uisgibtwzn"]} . "\n   \\_/  |_____|_____|\\___/ " . ${$yhxbrunnkluc};
+$bvudghjcjxg = "body0";
+echo ${${"GLOBALS"}["xsbepanksk"]} . "Bản Quyền: Group INDONESIA | Việt Hoá: TPTC" . ${${"GLOBALS"}["vuqdxzfzofi"]} . ${${"GLOBALS"}["vuqdxzfzofi"]};
+${${"GLOBALS"}["esqiepoy"]}    = "https://www.veeuapp.com/v1.0/incentive/tasks?access_token=440d99cc-ed0b-4685-ab07-c9c93b128897";
+${${"GLOBALS"}["lyxopjxorfi"]} = array(
+    "locale" => "in_ID",
+    "task_extra_info" => "",
+    "task_name" => "watch_video_ball_01",
+    "timezone" => "GMT+07:00"
+);
+${$bvudghjcjxg}                = json_encode(${${"GLOBALS"}["lyxopjxorfi"]}, true);
+${${"GLOBALS"}["xpivgmirdgk"]} = array();
+${$bwltzjkybw}[]               = "Host: www.veeuapp.com";
+"Connection: Keep-Alive";
+${"GLOBALS"}["xiykprmdwu"] = "info";
+"Accept-Encoding: gzip";
+$qguydorc = "header";
+"User-Agent: okhttp/3.10.0";
+${$qguydorc}              = array();
+${"GLOBALS"}["epvybnjag"] = "jres";
+$ioyqkvy                  = "total";
+$dxlodlptvwrq             = "result";
+${$fwvtmngl}[]            = "Content-Type: application/json";
+"charset=UTF-8";
+"Content-Length: 96";
+"Host: www.veeuapp.com";
+${"GLOBALS"}["wxuhwnckmeb"] = "konfir";
+$dqiuymijdwk                = "result";
+"Connection: Keep-Alive";
+"Accept-Encoding: gzip";
+"User-Agent: okhttp/3.10.0";
+require("config.php");
+$lktutag = "putih";
+echo ${${"GLOBALS"}["ktcmrjpgg"]} . "[>] $putih Bạn Đang Sở Hữu Tool Veeu Bản Hoàn Chỉnh\n";
+sleep(1);
+${${"GLOBALS"}["uyllmpi"]} = curl_init();
+curl_setopt_array(${${"GLOBALS"}["uyllmpi"]}, array(
+    CURLOPT_URL => "https://www.veeuapp.com/v1.0/me?access_token=" . ${${"GLOBALS"}["dfwqhrn"]},
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTPHEADER => ${${"GLOBALS"}["roiebxnsxa"]},
+    CURLOPT_SSL_VERIFYPEER => 0
+));
+${$dqiuymijdwk}                = curl_exec(${${"GLOBALS"}["uyllmpi"]});
+${${"GLOBALS"}["xwmaeugvkll"]} = curl_getinfo(${${"GLOBALS"}["jgntsehblf"]});
+curl_close(${${"GLOBALS"}["uyllmpi"]});
+${${"GLOBALS"}["rkonvnr"]}     = json_decode(${${"GLOBALS"}["qkzuumgkojl"]}, true);
+$pufcpmngpr                    = "jres";
+${${"GLOBALS"}["otwewgry"]}    = ${${"GLOBALS"}["xiykprmdwu"]}["http_code"];
+${${"GLOBALS"}["jfmliyqkqv"]}  = ${${"GLOBALS"}["wypwuqut"]}["user"]["nickname"];
+${${"GLOBALS"}["lxrbwmtffgc"]} = ${${"GLOBALS"}["gxfngkrscu"]}["user"]["email"];
+${${"GLOBALS"}["bditqkdgmk"]}  = ${$pufcpmngpr}["user"]["user_id"];
+${${"GLOBALS"}["miknrswr"]}    = ${${"GLOBALS"}["epvybnjag"]}["user"]["invite_code"];
+$blkvgmocymq                   = "lpoin";
+if (${${"GLOBALS"}["otwewgry"]} == "200") {
+    ${"GLOBALS"}["dpfdrlvlp"] = "ijo";
+    $istmnyuui                = "putih";
+    $tkwwmqdmbj               = "user";
+    echo ${${"GLOBALS"}["zwllmooqrysu"]} . "[*] " . ${${"GLOBALS"}["xsbepanksk"]} . "Trạng Thái Kết Nối: " . ${${"GLOBALS"}["dpfdrlvlp"]} . "Thành Công !!!\n";
+    echo ${$istmnyuui} . "➡️ Tên Tài Khoản: " . ${${"GLOBALS"}["ulllnbw"]} . ${$tkwwmqdmbj} . ${${"GLOBALS"}["xsbepanksk"]} . "\n💰 Email Của Bạn: $biru" . ${${"GLOBALS"}["txniqoiut"]} . "\n";
+} else {
+    ${"GLOBALS"}["mhermp"]     = "kuning";
+    ${"GLOBALS"}["gdeacscico"] = "putih";
+    ${"GLOBALS"}["iitskwtqtu"] = "t";
+    echo ${${"GLOBALS"}["xsbepanksk"]} . "Kết Nối: " . ${${"GLOBALS"}["bseinyhrik"]} . "Thất Bại !" . ${${"GLOBALS"}["iitskwtqtu"]} . ${${"GLOBALS"}["gdeacscico"]} . " | Thông Điệp: " . ${${"GLOBALS"}["mhermp"]} . "Sai Mã Token Vui Lòng Coppy Lại Thật Chính Xác" . ${${"GLOBALS"}["vuqdxzfzofi"]};
+    exit;
+}
+${${"GLOBALS"}["iwubhgkpyhn"]} = curl_init();
+${$blkvgmocymq}                = "https://www.veeuapp.com/v1.0/incentive/point?access_token=" . ${${"GLOBALS"}["dfwqhrn"]} . "&timezone=GMT%2B07%3A00&locale=in_ID";
+curl_setopt(${${"GLOBALS"}["sehhepb"]}, CURLOPT_URL, ${${"GLOBALS"}["yklfldlhi"]});
+curl_setopt(${${"GLOBALS"}["iwubhgkpyhn"]}, CURLOPT_RETURNTRANSFER, true);
+curl_setopt(${${"GLOBALS"}["iwubhgkpyhn"]}, CURLOPT_SSL_VERIFYPEER, 0);
+${$dxlodlptvwrq} = curl_exec(${${"GLOBALS"}["imqpetf"]});
+${$qejwhiicd}    = curl_getinfo(${$oesvize});
+curl_close(${${"GLOBALS"}["iwubhgkpyhn"]});
+${${"GLOBALS"}["mmhtluk"]}     = json_decode(${${"GLOBALS"}["qkzuumgkojl"]}, true);
+${$ioyqkvy}                    = ${${"GLOBALS"}["mmhtluk"]}["cumulative_point"];
+${${"GLOBALS"}["qajrktmxr"]}   = ${$tdhaqebtcem}["today_revenue_point"];
+${${"GLOBALS"}["qqqrbbeyuls"]} = ${$cwilbt}["http_code"];
+if (${${"GLOBALS"}["jfxbmbehrevb"]} == "200") {
+    ${"GLOBALS"}["hrjpcsfi"]    = "t";
+    ${"GLOBALS"}["ovqutmarhhz"] = "ijo";
+    $wqyagix                    = "total";
+    echo ${${"GLOBALS"}["xsbepanksk"]} . "💰Tổng Coin: " . ${${"GLOBALS"}["zwllmooqrysu"]} . ${$wqyagix} . ${${"GLOBALS"}["xsbepanksk"]} . " Coin Kiếm Hôm Nay (lỗi không hiển thị): " . ${${"GLOBALS"}["ovqutmarhhz"]} . ${${"GLOBALS"}["fkjnalv"]} . ${${"GLOBALS"}["hrjpcsfi"]} . ${${"GLOBALS"}["vuqdxzfzofi"]};
+}
+${${"GLOBALS"}["abfeuk"]} = readline(${$lktutag} . "[?] $turkis Viết [y] để đồng ý (y/n): ");
+if (${${"GLOBALS"}["abfeuk"]} == "y" OR ${${"GLOBALS"}["wxuhwnckmeb"]} == "Y") {
+    sleep(1);
+    $oyhdvkfdngxc = "kuning";
+    echo ${${"GLOBALS"}["ktcmrjpgg"]} . "[>] $putih Cảm Ơn Bạn Đã Dùng Tool Veeu Của Chúng Tôi !!!\n";
+    sleep(1);
+    echo ${$oyhdvkfdngxc} . "[>] $putih Đừng Đăng Ký Nhiều Nick 1 Máy Để Tránh Bị Khoá Nick\n";
+    sleep(1);
+} else {
+    echo ${${"GLOBALS"}["bseinyhrik"]} . "[!] " . ${${"GLOBALS"}["xsbepanksk"]} . "Thất Bại Là Mẹ Thành Công Đừng Nản\n";
+    exit;
+}
+exit;
+?>
